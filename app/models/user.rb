5b2ec998ae9545ad9c1df72136a83df49943ab97
johnnykramer <johnnyfearmekramer@gmail.com>
2016-09-29 07:59:39 +0000

    model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :name, :password, presence: true, length: { maximum: 15 }
     validates :email, presence: true, uniqueness: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
-    validates :port, presence: true, uniqueness: true, numericality: { greater_than: 1500, less_than: 2000 }, length: { is: 4 }
+    validates :port, presence: true, uniqueness: true, numericality: { greater_than: 1000, less_than: 9000 }, length: { is: 4 }
 end
